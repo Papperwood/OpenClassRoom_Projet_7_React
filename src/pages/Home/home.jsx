bfa1eb3c39439f0fdd5e695e365cfcb80edf6c6b
Papperwood <mauryalexandre.fgmail.com>
2023-04-25 11:43:38 +0200

    refactor : pages

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Home page
+          </h1>
+        </header>
+      </div>
+    );
+  }
+  
+export default Home;
