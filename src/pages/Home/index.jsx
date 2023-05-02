29c500aa0c54db83be1f45f313540d4533824bc4
Papperwood <mauryalexandre.fgmail.com>
2023-05-02 22:56:26 +0200

    refactor : home page banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,18 @@
 import "../css/normalize.css";
 import "../css/home.css";
+import IMG from "../../assets/IMG.png";
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 
 export default function Home() {
-	return (
-		<div className='home'>
-			<Header />
-			<Footer />
-		</div>
-	)
+  return (
+    <div className="home">
+      <Header />
+      <div className="home-container">
+        <img src={IMG} alt="Background" className="background-image" />
+        <div className="overlay-text">Chez vous, partout et ailleurs</div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
