c9313e3241601be3f0c52e3f8e32dbdcef144c43
Papperwood <mauryalexandre.fgmail.com>
2023-05-07 18:01:24 +0200

    style : collapse in logement

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,10 +1,21 @@
 import "../css/logement.css";
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import Dropdown from "../../components/dropdown/dropdown";
 import kasaList from "../../data/kasaList.json";
 
-
 export default function Logement() {
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+
+  const toggleDropdown1 = () => {
+    setIsOpen1(!isOpen1);
+  };
+
+  const toggleDropdown2 = () => {
+    setIsOpen2(!isOpen2);
+  };
+
   const { id } = useParams();
   const [property, setProperty] = useState(null);
 
@@ -17,10 +28,28 @@ export default function Logement() {
     return <div>Loading...</div>;
   }
 
+  const equipments = property.equipments ? property.equipments.join(", ") : "";
+  const description = property.description || "";
+
   return (
     <div>
       <h1>{property.title}</h1>
-      {/* Affichez les autres informations de la propriété ici */}
+      <div className="logement-container">
+        <div className="dropdown-container" style={{ display: "flex" }}>
+          <Dropdown
+            title="Description"
+            content={description}
+            isOpen={isOpen2}
+            toggleDropdown={toggleDropdown2}
+          />
+          <Dropdown
+            title="Équipements"
+            content={equipments}
+            isOpen={isOpen1}
+            toggleDropdown={toggleDropdown1}
+          />
+        </div>
+      </div>
     </div>
   );
 }
