e7c6f4d501dfcf362e30f9da9a270211450eade9
Papperwood <mauryalexandre.fgmail.com>
2023-05-12 18:07:12 +0200

    style : star logement

--- src/components/grade/grade.js
+++ src/components/grade/grade.js
@@ -1,15 +1,15 @@
 import React from "react";
-import star from "../../assets/star.svg";
+import starFull from "../../assets/star-full.png";
+import starEmpty from "../../assets/star-empty.png";
 
 function Grade({ grade }) {
-
   return (
     <div className="Grade-wrapper">
       {[1,2,3,4,5].map((_, i) => (
         <img
           className={i < grade ? "star-full" : "star-empty"}
           key={"star-" + i}
-          src={star}
+          src={i < grade ? starFull : starEmpty}
           alt={i < grade ? "Full Star Grade" : "Empty Star Grade"}
         />
       ))}
